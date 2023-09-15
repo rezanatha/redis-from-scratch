5f923762ff7b576954f4bf14f52d6aaa8c7604fd
rezanatha <wnrezaldi@gmail.com>
2023-09-15 12:07:18 +0700

    adding protocol to the request system

--- src/client.c
+++ src/client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -6,9 +7,89 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <signal.h>
+#include <assert.h>
 
 static void msg (const char* msg) {
 	fprintf(stderr, "%s\n", msg);
+}
+
+static void errmsg (const char* msg) {
+	fprintf(stderr, "[%d] %s ... %s\n", errno, strerror(errno) ,msg);
+}
+
+static int32_t read_full (int fd, char* buf, size_t n) {
+    while (n > 0) {
+		ssize_t rv = read(fd, buf, n);
+		if (rv <= 0) {
+			return -1;
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
+	}
+	return 0;
+}
+
+static int32_t write_all (int fd, const char* buf, size_t n) {
+	while (n > 0) {
+		ssize_t rv = write(fd, buf, n);
+		if (rv <= 0) {
+			return -1;
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
+	}
+	return 0;
+}
+
+const size_t k_max_msg = 4096;
+static int32_t query (int fd, const char* text) {
+    uint32_t len = (uint32_t)strlen(text);
+    if (len > k_max_msg) {
+        return -1;
+    }
+    // write
+    char wbuf[4+k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    int32_t err = write_all(fd, wbuf, 4 + len);
+    if (err) {
+        return err;
+    }
+
+    //4 bytes header
+    char rbuf[4+k_max_msg+1];
+    errno = 0;
+    err = read_full(fd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }    
+
+	memcpy(&len, rbuf, 4); //assuming little endian
+	if (len > k_max_msg) {
+		msg("too long");
+		return -1;
+	}
+
+    //reply body
+    err = read_full(fd, &rbuf[4], len);
+	if (err) {
+		msg("read() error");
+		return err;
+	}
+
+    //do something
+	rbuf[4 + len] = '\0';
+	printf("Server says: %s\n", &rbuf[4]);
+    return 0;
+
 }
 
 int talk (int connfd) {
@@ -16,7 +97,7 @@ int talk (int connfd) {
 	write(connfd, write_buffer, strlen(write_buffer));
 	char read_buffer[32] = {};
 	if(read(connfd, read_buffer, sizeof(read_buffer)-1) < 0) {
-		printf("read() error %s... \n", strerror(errno));
+        errmsg("read error");
 		return -1;
 	}
 	printf("Server says: %s \n", read_buffer);
@@ -27,7 +108,8 @@ int main() {
     setbuf(stdout, NULL);
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (client_fd == -1) {
-        printf("Socket creation failed: %s...\n", strerror(errno));
+        //printf("Socket creation failed: %s...\n", strerror(errno));
+        errmsg("Socket creation failed");
 		return 1;
     }
 
@@ -39,12 +121,26 @@ int main() {
     int rv = connect(client_fd, (const struct sockaddr *) &serv_addr, sizeof(serv_addr));
     if (rv) {
         //printf("Unable to connect to server: %d %s...\n", errno, strerror(errno));
-        msg("Unable to connect to server");
+        errmsg("Unable to connect to server");
         return 1;
     }
     printf("Connected to server. \n");
 
-    talk(client_fd);
+    //talk(client_fd);
+    int32_t err = query(client_fd, "hello1");
+    if (err) {
+        goto L_DONE;
+    }
+    err = query(client_fd, "hello2");
+    if (err) {
+        goto L_DONE;
+    }
+    err = query(client_fd, "hello there baby");
+    if (err) {
+        goto L_DONE;
+    }
+
+L_DONE:
     close(client_fd);
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,6 +12,10 @@
 
 static void msg (const char* msg) {
 	fprintf(stderr, "%s\n", msg);
+}
+
+static void errmsg (const char* msg) {
+	fprintf(stderr, "[%d] %s ... %s\n", errno, strerror(errno) ,msg);
 }
 
 static int respond_client(int connfd){
@@ -26,9 +31,14 @@ static int respond_client(int connfd){
 	return 0;
 }
 
+const size_t k_max_msg = 4096;
+
 static int32_t read_full (int fd, char* buf, size_t n) {
+	// read() will read all data that is available on the kernel
+	// read_full() ensures we only read n bytes of data
     while (n > 0) {
 		ssize_t rv = read(fd, buf, n);
+		printf("rv: %ld\n", rv);
 		if (rv <= 0) {
 			return -1;
 		}
@@ -52,6 +62,51 @@ static int32_t write_all (int fd, const char* buf, size_t n) {
 	return 0;
 }
 
+static int32_t one_request (int connfd) {
+	// read 4 bytes header
+	char rbuf[4 + k_max_msg + 1] = {};
+
+	errno = 0;
+	int32_t err = read_full(connfd, rbuf, 4); /* reading 4 bytes */
+	
+	if (err) {
+		if (errno == 0) {
+			msg("EOF");
+		} else {
+			msg("read() error ");
+		}
+
+		return err;
+	}
+	uint32_t len = 0;
+	memcpy(&len, rbuf, 4); //assuming little endian
+	printf("len: %d\n", len);
+	if (len > k_max_msg) {
+		msg("too long");
+		return -1;
+	}
+
+	// request body
+	err = read_full(connfd, &rbuf[4], len);
+	if (err) {
+		msg("read() error");
+		return err;
+	}
+
+	// do something
+	rbuf[4 + len] = '\0';
+	printf("Client says: %s\n", &rbuf[4]);
+
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+}
+
 
 int main() {
 	// Disable output buffering
@@ -61,7 +116,7 @@ int main() {
 	// fd = file descriptor, refers to something in an unix kernel (e.g., TCP connection, file, listening port)
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
+		errmsg("Socket creation failed");
 		return 1;
 	}
 
@@ -69,7 +124,7 @@ int main() {
 	// ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+		errmsg("SO_REUSEPORT failed");
 		return 1;
 	}
 	
@@ -81,13 +136,14 @@ int main() {
 	
 	//bind
 	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
+		errmsg("Bind failed");
+		
 		return 1;
 	}
 
     //listen
 	if (listen(server_fd, SOMAXCONN) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
+		errmsg("Listen failed");
 		return 1;
 	}
 	
@@ -105,12 +161,13 @@ int main() {
 	    }
 	    printf("Client connected\n");
         while (1) {
-			if (respond_client(client_fd) < 0) {
-	    	    printf("Unable to respond to client properly \n");
+			int32_t err = one_request(client_fd);
+			if (err) {
+	    	    //printf("Unable to respond to client properly \n");
 				break;
 	        } 
 		}
-		close(server_fd);
+		close(client_fd);
 	}
 	
 	return 0;
